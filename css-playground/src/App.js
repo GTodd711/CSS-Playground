670dd9c88b753fcb721507b95c6433b7e2fc0c15
GTodd711 <150283557+GTodd711@users.noreply.github.com>
2024-06-24 13:26:21 -0700

    had one idea liked it got another liked it more and guess what yup it happened again so not a lot going on but oh well

--- css-playground/src/App.js
+++ css-playground/src/App.js
@@ -1,23 +1,42 @@
+import galaxy from './galaxy.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img className="App-logo" alt="Gavin Todd" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+          <div className="orbit-container"></div>
+            <img src={galaxy} className="App-logo" alt="Galaxy Background" />
+            <div className="overlay">
+            <h1>Gavin Todd</h1>
+        <div className="orbit">
+          <div className="planet" id="mercury"></div>
+          <div className="planet" id="venus"></div>
+          <div className="planet" id="earth"></div>
+          <div className="planet" id="mars"></div>
+          <div className="planet" id="jupiter"></div>
+          <div className="planet" id="saturn"></div>
+          <div className="planet" id="uranus"></div>
+          <div className="planet" id="neptune"></div>
+        </div>
+        <article
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/GTodd711"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+          My Github
+        </article> 
+        </div>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
       </header>
-    </div>
+
+
+      </div>
+
+
   );
 }
 
